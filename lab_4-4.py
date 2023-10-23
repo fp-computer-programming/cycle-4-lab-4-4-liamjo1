e33f0df51822baefd296732228b0e944e8686ef5
p24lohara <p24lohara@fairfieldprep.org>
2023-10-22 21:47:32 -0400

    Lab 4-4 Submission

--- lab_4-4.py
+++ lab_4-4.py
@@ -13,3 +13,23 @@ Create a variable storing you first name written in lowercase. Using a string me
 Use the split method to split the following sentence at each comma: "I wish, I wish, I was a fish."
 
 """
+my_word = "flibbertigibbet"
+# input word as variable - O'Hara
+
+my_index = my_word.find("t")
+# find where t is in the word - O'Hara
+
+print(my_word[my_index + 1])
+# print letter after t in the index - O'Hara
+
+first_name = "liam"
+# store first name in lowercase - O'Hara
+
+print (first_name.upper())
+# make variable uppercase - O'Hara
+
+my_string ="I wish, I wish, I was a fish."
+# declare string - O'Hara
+
+print(my_string.split(sep=','))
+# split string at commas - O'Hara
